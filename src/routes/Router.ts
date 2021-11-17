98b20cde382c1128b7a1bf9cbd52688f21c9b568
saddam-satria <ardhisaddam@gmail.com>
2021-11-17 11:20:33 +0700

    add: endpoint detail book

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { welcomeApi, addBooks, getAllBooks, deleteBook } from '../controllers/API';
+import { welcomeApi, addBooks, getAllBooks, deleteBook, detailBook } from '../controllers/API';
 
 const router = Router();
 
 router.route('/api/v1').get(welcomeApi);
 router.route('/api/v1/books').post(addBooks).get(getAllBooks);
-router.route('/api/v1/books/:id').delete(deleteBook);
+router.route('/api/v1/books/:id').delete(deleteBook).get(detailBook);
 
 export default router;
