a2b982ffef306c5c26e659c5013bb4d29e20dbec
saddam-satria <ardhisaddam@gmail.com>
2021-11-18 17:30:29 +0700

    add:put method update book

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { welcomeApi, addBooks, getAllBooks, deleteBook, detailBook } from '../controllers/API';
+import { welcomeApi, addBooks, getAllBooks, deleteBook, detailBook, updateBook } from '../controllers/API';
 
 const router = Router();
 
 router.route('/api/v1').get(welcomeApi);
 router.route('/api/v1/books').post(addBooks).get(getAllBooks);
-router.route('/api/v1/books/:id').delete(deleteBook).get(detailBook);
+router.route('/api/v1/books/:id').delete(deleteBook).get(detailBook).put(updateBook);
 
 export default router;
